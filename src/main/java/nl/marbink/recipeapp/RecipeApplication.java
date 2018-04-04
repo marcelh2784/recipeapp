93f884843ef3dc32e96aa16ca0416e83c57597f4
MH <marcelh2784@gmail.com>
2018-04-04 16:47:34 +0200

    Chapter 99: Assignment - Create One to One Relationship

--- src/main/java/nl/marbink/recipeapp/RecipeApplication.java
+++ src/main/java/nl/marbink/recipeapp/RecipeApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RecipeappApplication {
+public class RecipeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RecipeappApplication.class, args);
+		SpringApplication.run(RecipeApplication.class, args);
 	}
 }
