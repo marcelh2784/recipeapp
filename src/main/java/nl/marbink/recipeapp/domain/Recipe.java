f0718f142b0e8d28416d88905868af675a99c6c8
MH <marcelh2784@gmail.com>
2018-04-04 19:14:12 +0200

    Chapter 108: Protips

--- src/main/java/nl/marbink/recipeapp/domain/Recipe.java
+++ src/main/java/nl/marbink/recipeapp/domain/Recipe.java
@@ -114,6 +114,7 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+        notes.setRecipe(this);
     }
 
     public Set<Ingredient> getIngredients() {
@@ -139,4 +140,12 @@ public class Recipe {
     public void setCategories(Set<Category> categories) {
         this.categories = categories;
     }
+
+    public Recipe addIngredient(Ingredient ingredient) {
+        ingredient.setRecipe(this);
+        this.ingredients.add(ingredient);
+        return this;
+    }
+
+
 }
